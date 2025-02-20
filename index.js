358c827025a90e7b4ebb99a175240fa25d8b84ab
shiragelb <shi9ra@gmail.com>
2025-02-20 09:41:20 +0200

    Third commit adding project contact and some interactive features.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
+
+    // a functionality of fading and show in the beginin of the page
+    document.body.classList.add("is-preload"); // Add is-preload class
+
+    const heading = document.querySelector("h1.text-body-emphasis-1");
+
+    if (heading) {
+        if(localStorage.getItem('headingShown') !== 'true'){
+            setTimeout(function () {
+                heading.classList.add("show");
+                localStorage.setItem('headingShown', 'true');
+            }, 500);
+        } else {
+            heading.classList.add("show");
+        }
+    }
+
+    window.addEventListener("load", function(){
+        document.body.classList.remove("is-preload"); //remove is preload class
+    });
+
+    //linking all the buttoms to their sections
     const scrollButton = document.getElementById("scroll-button");
 
     if (scrollButton) {
